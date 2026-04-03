7e83c4353be04eeecfe4e1c674d093083777ca1d
Arpita Das <arpita1477@gmail.com>
2026-04-03 22:42:04 +0600

    Create Algorithm4_1prime.cpp

---
+++ Level-2,Semester-1/CIT_212/Chapter_04/Algorithm4_1prime.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int LA[]={10,20,30,40,50};
+    int LB=0,UB=4;
+    for(int k = LB;k<=UB;k++){
+        cout<<LA[k]<<" ";
+    }
+    return 0;
+}
